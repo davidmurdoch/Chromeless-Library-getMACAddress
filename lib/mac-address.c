2f86ce3abf8172572d370c9b3777c6403bb188a3
David Murdoch <musicisair@yahoo.com>
2011-05-19 00:43:12 +0000

    Initial COmmit

---
+++ lib/mac-address.c
@@ -0,0 +1,27 @@
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <netdb.h>
+#include <stdio.h>
+
+char *getMACAddress()
+{
+  #define MAC_STRING_LENGTH 13
+  char *ret = malloc(MAC_STRING_LENGTH);
+  struct ifreq s;
+  int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+
+  strcpy(s.ifr_name, "eth1");
+  if (fd >= 0 && ret && 0 == ioctl(fd, SIOCGIFHWADDR, &s))
+  {
+    int i;
+    for (i = 0; i < 6; ++i)
+      snprintf(ret+i*2,MAC_STRING_LENGTH-i*2,"%02x",(unsigned char) s.ifr_addr.sa_data[i]);
+  }
+  else
+  {
+    perror("malloc/socket/ioctl failed");
+    return(ret);
+  }
+  return(ret);
+}
